2defe45d396d546e2c36ad83468feca6fbdf7f54
Avstel <it.nathan.demissie@gmail.com>
2025-11-02 10:35:26 +0300

    Update signals.py

--- properties/signals.py
+++ properties/signals.py
@@ -1,1 +1,9 @@
-from django.db.models.signals import post
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.core.cache import cache
+from properties.models import Property
+
+@receiver(post_save, sender=Property)
+@receiver(post_delete, sender=Property)
+def invalidate_property_cache(sender, **kwargs):
+    cache.delete('all_properties')
